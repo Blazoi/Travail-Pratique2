444715531a5162cd1d99a6f9e307baa1d2f79361
Blazoi <133470741+Blazoi@users.noreply.github.com>
2024-05-08 08:32:20 -0400

    Update Case.java

--- src/Case.java
+++ src/Case.java
@@ -1,6 +1,6 @@
 public abstract class Case {
     private final String type;
-    private String nom;
+    private String nom; int test = 0;
     private int montantdelacase;
     public Case (String type, String nom, int montantapayer){
         this.type = type;
